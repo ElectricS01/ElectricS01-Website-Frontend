aa96811c4bb9d95642938ae00f1880a00a3dc0af
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-02-01 16:21:55 +1100

    1.27

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,44 @@
 <template>
   <header>
-    <div class="topnav" id="mobile-topnav">
+    <div class="topnav" id="mobile-topnav" v-if="navbarShown">
       <router-link class="main" to="/">Home</router-link>
-      <router-link to="/tonkgame" :class="{ active: active('/tonkgame') }">TonkGame</router-link>
-      <router-link to="/calculator" :class="{ active: active('/calculator') }">Calculator</router-link>
-      <router-link to="/tetris" :class="{ active: active('/tetris') }">Tetris</router-link>
+      <router-link to="/tonkgame" :class="{ active: active('/tonkgame') }"
+        >TonkGame</router-link
+      >
+      <router-link to="/calculator" :class="{ active: active('/calculator') }"
+        >Calculator</router-link
+      >
+      <router-link to="/tetris" :class="{ active: active('/tetris') }"
+        >Tetris</router-link
+      >
       <a href="https://bettercompassclub.netlify.app">BetterCompass Club</a>
       <a class="icon" @click="responsive_topnav()">☰</a>
     </div>
   </header>
   <main>
-    <router-view/>
+    <router-view />
   </main>
 </template>
 
 <script>
 export default {
   name: "App",
-  methods:{
+  methods: {
     active(route) {
-      return route === this.$route.path;
+      return route === this.$route.path
     },
     responsive_topnav() {
-      const responsive_topnav = document.getElementById("mobile-topnav");
+      const responsive_topnav = document.getElementById("mobile-topnav")
       if (responsive_topnav.className === "topnav") {
-        responsive_topnav.className += " responsive";
+        responsive_topnav.className += " responsive"
       } else {
-        responsive_topnav.className = "topnav";
+        responsive_topnav.className = "topnav"
       }
+    }
+  },
+  computed: {
+    navbarShown() {
+      return this.$route.path !== "/test"
     }
   }
 }
