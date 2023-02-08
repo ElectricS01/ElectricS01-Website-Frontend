83427f087201a8f85575d609a7a73a3a00e75964
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-02-08 23:32:54 +1100

    1.41

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-const isLoggedIn = () => {
-  this.axios.get("/api/user").then((res) => {
-    this.loggedIn = res.data
-  })
-}
-
 const routes = [
   {
     path: "/",
@@ -35,50 +29,22 @@ const routes = [
   {
     path: "/account",
     name: "account",
-    component: () => import("../views/BetterTest/Account.vue"),
-    beforeEnter: (to, from, next) => {
-      if (!isLoggedIn) {
-        next("/test")
-      } else {
-        next()
-      }
-    }
+    component: () => import("../views/BetterTest/Account.vue")
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("../views/BetterTest/Login.vue"),
-    beforeEnter: (to, from, next) => {
-      if (isLoggedIn) {
-        next("/test")
-      } else {
-        next()
-      }
-    }
+    component: () => import("../views/BetterTest/Login.vue")
   },
   {
     path: "/register",
     name: "register",
-    component: () => import("../views/BetterTest/Register.vue"),
-    beforeEnter: (to, from, next) => {
-      if (isLoggedIn) {
-        next("/test")
-      } else {
-        next()
-      }
-    }
+    component: () => import("../views/BetterTest/Register.vue")
   },
   {
     path: "/resetpassword",
     name: "resetpassword",
-    component: () => import("../views/BetterTest/ResetPassword.vue"),
-    beforeEnter: (to, from, next) => {
-      if (!isLoggedIn) {
-        next("/test")
-      } else {
-        next()
-      }
-    }
+    component: () => import("../views/BetterTest/ResetPassword.vue")
   },
   {
     path: "/deez",
