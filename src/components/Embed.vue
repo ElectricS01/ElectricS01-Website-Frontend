918ae8a230c5584fbc8dcf6b71575a490e21edd3
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-02-12 20:21:44 +1100

    Revert "Revert "1.49""
    This reverts commit 9d4020053c0a6342863377bb908f0081499a66e9.

---
+++ src/components/Embed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="embed.type === 'image'">
+    <img
+      :src="embed.mediaProxyLink"
+      style="max-width: 400px; max-height: 300px"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Embed",
+  props: ["embed"]
+}
+</script>
