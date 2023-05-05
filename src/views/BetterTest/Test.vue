630aaf53d80df5cb4aa56527556e62f0c2cfc9e2
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-05-05 15:27:35 +1000

    1.112

--- src/views/BetterTest/Test.vue
+++ src/views/BetterTest/Test.vue
@@ -121,7 +121,7 @@
             v-if="
               showUser.friendRequests &&
               showUser.id !== loggedIn.id &&
-              !showUser?.friend[0]?.status
+              !showUser.friendStatus
             "
             class="profile-button-add"
             style="color: #47bf4c; width: 100%"
@@ -139,7 +139,7 @@
           <button
             v-if="
               showUser.id !== loggedIn.id &&
-              showUser?.friend[0]?.status === 'accepted'
+              showUser.friendStatus === 'accepted'
             "
             class="profile-button-remove"
             style="color: #ff2f2f; width: 100%"
@@ -156,8 +156,7 @@
           </button>
           <button
             v-if="
-              showUser.friendRequests &&
-              showUser?.friend[0]?.status === 'pending'
+              showUser.friendRequests && showUser.friendStatus === 'pending'
             "
             class="profile-button-pending"
             style="color: #808080; width: 100%"
@@ -175,7 +174,7 @@
           <button
             v-if="
               showUser.id !== loggedIn.id &&
-              showUser?.friend[0]?.status === 'incoming'
+              showUser.friendStatus === 'incoming'
             "
             class="profile-button-pending"
             style="color: #47bf4c; width: 100%"
