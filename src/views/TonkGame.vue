f2b539d63215500d3b1d6faab66a96bb25d0e4f4
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-09-11 12:43:00 +1000

    1.163

--- src/views/TonkGame.vue
+++ src/views/TonkGame.vue
@@ -10,7 +10,7 @@
       height="540"
       width="960"
     ></iframe>
-    <h3>TonkGame version 4.2.9.2 (beta 10)</h3>
+    <h3>TonkGame version 4.2.10.0 (beta 11)</h3>
     <p class="message-text-medium-gray">
       My first ever coding project, a simple 2D Top Down Shooter made
       with GameMaker Studio
