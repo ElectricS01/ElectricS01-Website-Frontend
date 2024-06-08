74b72134ae7f50f943c8f3e08ff313c407c7efec
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-06-08 15:12:35 +1000

    1.213.2

--- src/views/BetterComms/Account.vue
+++ src/views/BetterComms/Account.vue
@@ -437,7 +437,7 @@
               <router-link to="/">ElectricS01</router-link>
             </div>
             <div class="settings-spacer" />
-            <div>Version: 1.213.0</div>
+            <div>Version: 1.213.2</div>
             <div class="settings-spacer" />
             <div>Backend name: {{ serverName }}</div>
           </div>
