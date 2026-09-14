f05dc05431435b973be650c479359c54b32b1e9f
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2026-09-15 09:05:46 +1000

    1.251.12

--- src/components/account/About.vue
+++ src/components/account/About.vue
@@ -10,7 +10,7 @@
     <router-link to="/">ElectricS01</router-link>
   </div>
   <div class="settings-spacer" />
-  <div>Version: 1.251.11</div>
+  <div>Version: 1.251.12</div>
   <div class="settings-spacer" />
   <div>Build date: {{ dayjsLong(buildDate) }}</div>
   <div class="settings-spacer" />
